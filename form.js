06df9cd237cab04d664edfdc1b5b09bd7729ad50
=abhishek1kumar <=abhishekgate33@gmail.com>
2023-08-01 00:43:19 +0530

    color to list item and bold

--- form.js
+++ form.js
@@ -10,3 +10,12 @@ var item =document.getElementById('title1')
 console.log(item);
 item.style.color = 'green'
 item.style.fontWeight='bold'
+
+var items =document.getElementsByClassName('list-group-item')
+items[2].style.background ='green'
+//items.style.fontWeight = 'bold'
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+    items[i].style.color ='red'
+}
