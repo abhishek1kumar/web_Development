9a61a01d487e4daba3b6479c52310d72fe61d78e
=abhishek1kumar <=abhishekgate33@gmail.com>
2023-08-01 00:18:06 +0530

    firstcomment

---
+++ form.js
@@ -0,0 +1,12 @@
+console.log('hello from from')
+var headerTitle =document.getElementById('header-title')
+console.log(headerTitle);
+//headerTitle.textContent ='hello'
+headerTitle.style.borderBottom='solid 5px #000';
+var header =document.getElementById('main-header');
+header.style.borderBottom ='solid 8px #900';
+var item =document.getElementById('title1')
+
+console.log(item);
+item.style.color = 'green'
+item.style.fontWeight='bold'
