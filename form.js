3491db596913be6182244dedc9316cfa5f29f14f
=abhishek1kumar <=abhishekgate33@gmail.com>
2023-08-01 13:32:28 +0530

    li tag added and alsochanges magde by using byclassname and tagname

--- form.js
+++ form.js
@@ -18,4 +18,21 @@ for(var i=0;i<items.length;i++)
 {
     items[i].style.fontWeight='bold';
     items[i].style.color ='red'
+
 }
+
+var li =document.getElementsByTagName('li')
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.fontWeight='bold';
+    li[i].style.color ='blue'
+}
+/* var ul =document.getElementsByTagName('ul');
+ul.li[4].innerHTML='<h1> add iten <h1>'
+li[5].innerHTML='<h2>item 6 </h2>'okay bro
+ */
+items[3].textContent='fruits'
+items[2].textContent='vegetable'
+//items[5].innerHtml ='<h1>item 5</h1>'
+li[4].textContent='egg'
+
